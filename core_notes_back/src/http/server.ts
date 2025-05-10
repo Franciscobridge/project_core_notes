4557d544df4a5390f5f98210be11dbeea5a9c2b2
Franciscobridge <167431867+Franciscobridge@users.noreply.github.com>
2025-05-10 13:10:22 -0700

    Core_Notes: incluindo a variavel de ambiente

--- core_notes_back/src/http/server.ts
+++ core_notes_back/src/http/server.ts
@@ -1,26 +1,29 @@
-import fastify from "fastify";
-import { CreateNote } from "../routes/create-note";
-import { UpdateNote } from "../routes/update-note";
-import cors from "@fastify/cors";
-import {
-  serializerCompiler,
-  validatorCompiler,
-} from "fastify-type-provider-zod";
-import { UpdateFavoriteNote } from "../routes/update-favorite-camp";
-import { DeleteNote } from "../routes/delete-note";
-import { UpdateColorNote } from "../routes/update-color-note";
-import { GetAllNotes } from "../routes/get-notes";
+import fastify from 'fastify';
+import cors from '@fastify/cors';
+import { CreateNote } from '../routes/create-note';
+import { UpdateNote } from '../routes/update-note';
+import { UpdateFavoriteNote } from '../routes/update-favorite-camp';
+import { DeleteNote } from '../routes/delete-note';
+import { UpdateColorNote } from '../routes/update-color-note';
+import { GetAllNotes } from '../routes/get-notes';
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 
 const app = fastify();
 
+// Garantindo que a variável de ambiente `PORT` seja um número
+const port = Number(process.env.PORT) || 3333; // Converte para número ou usa 3333 como fallback
+
+// Registro do CORS
 app.register(cors, {
   origin: true,
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
 });
 
+// Configura o compilador de validador e serializador
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+// Registrando rotas
 app.register(GetAllNotes);
 app.register(CreateNote);
 app.register(UpdateNote);
@@ -28,6 +31,7 @@ app.register(UpdateFavoriteNote);
 app.register(UpdateColorNote);
 app.register(DeleteNote);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("HTTP server running!");
+// Inicializando o servidor
+app.listen({ port }).then(() => {
+  console.log(`HTTP server running on port ${port}`);
 });
